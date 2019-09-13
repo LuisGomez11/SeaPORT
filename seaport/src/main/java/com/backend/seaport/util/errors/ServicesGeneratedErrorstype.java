f46fa95aacd0a194c02cbdd43c225bcb39fc4f4e
christian <pearson3135@gmail.com>
2019-09-13 13:10:45 -0500

    b

--- seaport/src/main/java/com/backend/seaport/util/errors/ServicesGeneratedErrorstype.java
+++ seaport/src/main/java/com/backend/seaport/util/errors/ServicesGeneratedErrorstype.java
@@ -3,19 +3,11 @@
  */
 package com.backend.seaport.util.errors;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import com.backend.seaport.model.ServicesGenerated;
-
-import lombok.Data;
-
 /**
  * @author christian
  *
  */
-@Data
+
 public class ServicesGeneratedErrorstype {
-	
-	
+
 };
